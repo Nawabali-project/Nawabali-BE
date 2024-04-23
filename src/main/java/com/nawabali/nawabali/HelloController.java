c5e71a16bf7d4902be2402dda42442196f77c7b5
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-04-23 16:27:01 +0900

    fix : HelloController 임의 수정 5

--- src/main/java/com/nawabali/nawabali/HelloController.java
+++ src/main/java/com/nawabali/nawabali/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/ping")
     public String check() {
-        return "Pong! 무중단배포 #4";
+        return "Pong! 무중단배포 성공! 0.1초 안에 Old/New 서버 교체 완료!";
     }
 }

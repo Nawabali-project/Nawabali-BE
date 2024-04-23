a66604a3ffb61cbc250bdd16fa81cf8423ac0759
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-04-23 16:01:14 +0900

    fix : HelloController 임의 수정 4

--- src/main/java/com/nawabali/nawabali/HelloController.java
+++ src/main/java/com/nawabali/nawabali/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/ping")
     public String check() {
-        return "Pong! 무중단배포 #3";
+        return "Pong! 무중단배포 #4";
     }
 }

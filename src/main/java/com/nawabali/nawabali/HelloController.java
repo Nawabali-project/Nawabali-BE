de7ae9d429fa2b277bc5878e9536c1f67b281893
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-05-06 17:59:54 +0900

    fix : HelloController 임의 수정 3

--- src/main/java/com/nawabali/nawabali/HelloController.java
+++ src/main/java/com/nawabali/nawabali/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/ping")
     public String check() {
-        return "Pong! 서버 업그레이드 이전 2";
+        return "Pong! 서버 업그레이드 이전 3";
     }
 }

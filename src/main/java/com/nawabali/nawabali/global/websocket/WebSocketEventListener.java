d87e95a162355674895a3aebfc28ad47d2f54255
minnieming <eunmilee9109.1@gmail.com>
2024-05-06 18:52:24 +0900

    FIX : 테스트위한 log

--- src/main/java/com/nawabali/nawabali/global/websocket/WebSocketEventListener.java
+++ src/main/java/com/nawabali/nawabali/global/websocket/WebSocketEventListener.java
@@ -1,6 +1,7 @@
 package com.nawabali.nawabali.global.websocket;
 
 import com.nawabali.nawabali.service.NotificationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.security.core.Authentication;
@@ -12,15 +13,14 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Slf4j
 @Component
 public class WebSocketEventListener {
 
     private final WebSocketChatRoomCount chatRoomCount;
-    private final NotificationService notificationService;
 
-    public WebSocketEventListener (WebSocketChatRoomCount chatRoomCount, NotificationService notificationService) {
+    public WebSocketEventListener (WebSocketChatRoomCount chatRoomCount) {
         this.chatRoomCount = chatRoomCount;
-        this.notificationService = notificationService;
     }
 
     @EventListener
@@ -41,6 +41,7 @@ public class WebSocketEventListener {
                 chatRoomId = matcher.group(1);
             }
         }
+        log.info("웹소켓 구독 완료");
         chatRoomCount.addUser(Long.valueOf(chatRoomId), email);
     }
 
@@ -59,6 +60,7 @@ public class WebSocketEventListener {
             String chatRoomIdString = chatRoomIdList.get(0);
             chatRoomId = Long.valueOf(chatRoomIdString);
         }
+        log.info("웹소켓 구독 끊김!!!!");
        chatRoomCount.outUser(Long.valueOf(chatRoomId),email);
     }
 }

551f80db13b409aeda204baf5b46916b26cd1f38
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-05-16 12:50:59 +0900

    fix : 디폴트 프로필이미지 서버주소 변경

--- src/main/java/com/nawabali/nawabali/constant/DefaultProfileImage.java
+++ src/main/java/com/nawabali/nawabali/constant/DefaultProfileImage.java
@@ -5,5 +5,5 @@ import lombok.Getter;
 @Getter
 public class DefaultProfileImage {
     public static String fileName = "동네방네 프로필.svg";
-    public static String imgUrl = "https://nawabalibucket.s3.ap-northeast-2.amazonaws.com/profileImage/3b6ad69e-73e8-443c-a14a-f5de2cbf6814.svg";
+    public static String imgUrl = "https://bucket-image-dongnaebangnae.s3.ap-northeast-2.amazonaws.com/profileImage/%EB%8F%99%EB%84%A4%EB%B0%A9%EB%84%A4+%ED%94%84%EB%A1%9C%ED%95%84.svg";
 }
